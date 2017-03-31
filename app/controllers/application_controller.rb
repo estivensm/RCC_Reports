e029b9f8b5c17aee28e8b4a643d90e44ea9cf42d
alejovelez10 <alejovelez10@gmail.com>
2017-03-31 09:39:43 -0500

    seguimientos

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,12 +1,20 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
-layout :layout_by_resource
-   def layout_by_resource
-    if user_signed_in?
-      'application'
-    else
+layout :layout_for_selection
+protected
+  def layout_for_selection
+    if controller_name == 'sessions'  || controller_name == 'passwords' 
       'home'
+    elsif controller_name == 'registrations'
+
+        if action_name == "new" || action_name == "create"
+            'home'
+            else
+            	'application'
+        end
+    	else
+      'application'
     end
   end
 end
